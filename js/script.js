f9d655ba4bbba234eea6663b8aaa3fe9cc7c2bec
rifkikarimramadhan <rifkikarimr@gmail.com>
2021-02-28 02:46:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ $(window).scroll(function() {
     $('.portfolio .thumbnail').each(function(i) {
       setTimeout(function() {
         $('.portfolio .thumbnail').eq(i).addClass('muncul');
-      }, 300 * (i+1))
+      }, 200 * (i+1))
     }); 
   }
 
