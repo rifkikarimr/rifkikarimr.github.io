d54309fdbf018253abb264190183ed57d6b32262
rifkikarimramadhan <rifkikarimr@gmail.com>
2021-02-28 02:41:00 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,35 @@ $('.page-scroll').on('click', function(e){
 
   e.preventDefault();
 });
+
+//about
+$(window).on('load', function(){
+  $('.pLeft').addClass('pMuncul');
+  $('.pRight').addClass('pMuncul');
+});
+
+// parallax
+$(window).scroll(function() {
+  var wScroll = $(this).scrollTop();
+
+  $('.jumbotron img').css({
+    'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+  });
+
+  $('.jumbotron h1').css({
+    'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+  });
+
+  $('.jumbotron p').css({
+    'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+  });
+
+  if(wScroll > $('.portfolio').offset().top - 10){
+    $('.portfolio .thumbnail').each(function(i) {
+      setTimeout(function() {
+        $('.portfolio .thumbnail').eq(i).addClass('muncul');
+      }, 300 * (i+1))
+    }); 
+  }
+
+});
